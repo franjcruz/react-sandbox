a532c1310fb6f0448377f2c359ae8814fa3b94bd
Fran Cruz <fjose.cruz@alten.es>
2021-05-19 16:34:45 +0200

    New component

---
+++ src/components/gameboardsList/GameboardsList.tsx
@@ -0,0 +1,12 @@
+import { FC } from "react"
+
+export interface GameboardsListProps {
+}
+
+export const GameboardsList: FC<GameboarsdListProps> = () => {
+  return (
+    <div className="site-layout-background" style={{ padding: 24, minHeight: 360 }}>
+      GameboardsList
+    </div>
+  )
+}

f653e94b9a4c931248c485cba33ec6b574337f27
Fran Cruz <fjose.cruz@alten.es>
2021-05-19 16:50:33 +0200

    Fix error with name

--- src/components/GameboardsList/GameboardsList.tsx
+++ src/components/GameboardsList/GameboardsList.tsx
@@ -3,7 +3,7 @@ import { FC } from "react"
 export interface GameboardsListProps {
 }
 
-export const GameboardsList: FC<GameboarsdListProps> = () => {
+export const GameboardsList: FC<GameboardsListProps> = () => {
   return (
     <div className="site-layout-background" style={{ padding: 24, minHeight: 360 }}>
       GameboardsList
